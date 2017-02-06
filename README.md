bdcf23e3b26a911c412d8989b11964391745fa47
Dmitry Ponyatov <dponyatov@gmail.com>
2017-02-06 21:44:04 +0400

    + doc/ skelex make compiler.dot
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ### uCompiler
 реализация компилятора для простейшего арифметического языка на flex/bison/c++
 
-(c) Dmitry Ponyatov <dponyatov@gmai.com>
+(c) Dmitry Ponyatov <<dponyatov@gmai.com>>
 
 github: https://github.com/ponyatov/uc
--- doc/00.mk
+++ doc/00.mk
@@ -1,2 +1,10 @@
 log.log: src.src ./exe.exe
 	./exe.exe < src.src > log.log && tail $(TAIL) log.log
+C = cpp.cpp ypp.tab.cpp lex.yy.c
+H = hpp.hpp ypp.tab.hpp
+./exe.exe: $(C) $(H)
+	$(CXX) -o $@ $(C)
+ypp.tab.cpp: ypp.ypp
+	bison $<
+lex.yy.c: lpp.lpp
+	flex $<
