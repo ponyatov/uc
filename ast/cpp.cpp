d91950df70d9f1e3df9c6ef9df72be1eb7b72fb7
Dmitry Ponyatov <dponyatov@gmail.com>
2017-02-08 10:16:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- ast/cpp.cpp
+++ ast/cpp.cpp
@@ -1,5 +1,5 @@
 #include "hpp.hpp"
-#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+#define YYERR "\n\n"<< yylineno <<":"<< msg <<"["<< yytext <<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
 int main() { return yyparse(); }
 
