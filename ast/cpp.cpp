2881fa3cdae194aa1b33c699400a33f0a4353d26
Dmitry Ponyatov <dponyatov@gmail.com>
2017-02-08 09:56:24 +0400

    * ast/
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- ast/cpp.cpp
+++ ast/cpp.cpp
@@ -1,2 +1,27 @@
 #include "hpp.hpp"
+#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
 int main() { return yyparse(); }
+
+Sym::Sym(string T,string V) { tag=T; val=V; }
+Sym::Sym(string V):Sym("sym",V){}
+Sym* Sym::push(Sym*o) { nest.push_back(o); return this; }
+
+string Sym::head() { ostringstream os;
+	os << "<"<< tag <<":"<< val <<"> #"<< this ; return os.str(); }
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
+	for (auto it=attr.begin(),e=attr.end();it!=e;it++) {
+		S += "\n"+pad(depth+1)+ it->first +" =";
+		S += it->second->dump(depth+2); }
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
+	return S; }
+
+Num::Num(string V):Sym("num","") { val=atof(V.c_str()); }
+string Num::head() { ostringstream os;
+	os << val <<" #"<< this ; return os.str(); }
+
+Op::Op(string V):Sym("op",V){}
+
+Fn::Fn(string V, FN F):Sym("fn",V) { fn=F; }
--- ast/hpp.hpp
+++ ast/hpp.hpp
@@ -2,26 +2,49 @@
 #define _H_HPP
 
 #include <iostream>
+#include <sstream>
 #include <cstdlib>
 #include <vector>
 #include <map>
 using namespace std;
 
-struct Sym {	// universal algebraic symbolic type, struct applies public
-	string tag;	// class/type marker, required for token elements
-	string val;	// value, string is universal can represent _any_ data
+struct Sym {		// universal algebraic symbolic type, struct applies public
+	string tag;			// class/type marker, required for token elements
+	string val;			// value, string is universal can represent _any_ data
 
-	Sym(string T,string V);	// <T:V> pair constructor
-	Sym(string V);			// token constructor
+	Sym(string T,string V);			// <T:V> pair constructor
+	Sym(string V);					// token constructor
 
-	vector<Sym*> nest;		// \ nest[]ed elements for AST tree
-	void push(Sym*o);		// / push new nested element
+	vector<Sym*> nest;				// \ nest[]ed elements for AST tree
+	Sym* push(Sym*o);				// / push new nested element
 
-	map<string,Sym*> attr;	// \ named attributes, you require it
+	map<string,Sym*> attr;			// \ named attributes, you require it
 
-
+	virtual string head();			// return "<T:V>" pair repr
+	string pad(int);				// pad output of tree elements
+	string dump(int depth=0);		// return tree dump
 };
 
-extern int yylex();			// \ 
+struct Num: Sym { Num(string);		// number tokens
+	double val; string head(); };
+struct Str: Sym { Str(string);		// 'string' tokens
+	string head(); };
+
+struct Vector: Sym { Vector(); };	// [vector]
+struct Op: Sym { Op(string); };		// operator
+
+typedef Sym*(*FN)(Sym*);					// \ [primitive] function
+struct Fn: Sym { Fn(string V, FN F=NULL);
+	FN fn; };								// /
+
+								// == lexer interface ==
+extern int yylex();				// get next token
+extern int yylineno;			// line number
+extern char* yytext;			// parsed lexeme (ASCIIZ string)
+#define TOC(C,X) { yylval.o = new C(yytext); return X; } // gen.token macro
+								// == syntax parser interface ==
+extern int yyparse();			// grammar parser
+extern void yyerror(string);	// error callback function
+#include "ypp.tab.hpp"			// shared lex/yacc token definitions
 
 #endif // _H_HPP
